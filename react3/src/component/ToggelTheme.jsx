41656d4dbb3be97b5e95487175474144be1b9b69
samane ghodrati <samane.ghodrati@gmail.com>
2025-05-29 17:52:49 -0900

    add

--- react3/src/component/ToggelTheme.jsx
+++ react3/src/component/ToggelTheme.jsx
@@ -1,14 +1,14 @@
-import "../app.css";
+import '../app.css';
 import { useState } from "react";
 export default function ToggleTheme(){
-    const [isDarkTheme,setDarkTheme]=useState("true");
+    const [isDarkTheme,setDarkTheme]=useState(true);
     const toggleTheme = () => {
         setDarkTheme(!isDarkTheme)
           }
     return(
         <div>
-<p></p>
-<button onClick={toggleTheme}>click</button>
+
+<button onClick={toggleTheme}><p className={isDarkTheme ? 'dark-theme' : 'light-theme'}>{isDarkTheme ? 'isdark' : 'islight'}</p></button>
 
         </div>
     )
