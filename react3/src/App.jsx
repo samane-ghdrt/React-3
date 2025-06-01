84f188301de77ec6b6b4e32d7ca1f8917102a712
samane ghodrati <samane.ghodrati@gmail.com>
2025-05-31 22:38:03 -0900

    add

--- react3/src/App.jsx
+++ react3/src/App.jsx
@@ -1,11 +1,13 @@
-import Counter from "./component/counter"
-import ToggleTheme from "./component/ToggelTheme"
+import Counter from "./component/counter";
+import ToggleTheme from "./component/ToggelTheme";
+import LikeAndDiselike from "./component/likeAndDiislike";
 
 export default function App(){
   return(
     <div>
 <Counter/>
 <ToggleTheme/>
+<LikeAndDiselike/>
     </div>
   )
 };
